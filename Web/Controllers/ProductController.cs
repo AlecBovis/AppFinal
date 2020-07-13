3bdd63cddb6595d78b6bc854b254d495905ae2dd
bryanaponte <bryan.aponte@tecsup.edu.pe>
2020-07-13 11:37:30 -0500

    correccion

--- Web/Controllers/ProductController.cs
+++ Web/Controllers/ProductController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
 
 namespace Web.Controllers
 {
-    public class ProductController : ApiController
+    public class ProductController : Controller
     {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
